a7fb485e826872f1933e0b276986363b27341bd6
Saleem Abdul Hasan <116128988+saleempeeran@users.noreply.github.com>
2022-10-20 19:16:55 -0500

    Update streamlit.app.py
    added new items

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,7 @@
 import streamlit
-streamlit.title('My Parents new healthy diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+
+
 
